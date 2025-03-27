27ce7eae26ed5d5f9decff34ae3a87cac19e0d38
Ekachai S <BBright1998@hotmail.com>
2025-03-27 14:45:44 +0700

    commit a

---
+++ go.mod
@@ -0,0 +1,37 @@
+module ebook
+
+go 1.22.3
+
+require github.com/bmaupin/go-epub v1.1.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/unidoc/pkcs7 v0.2.0 // indirect
+	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
+	github.com/unidoc/unitype v0.4.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gofrs/uuid v3.1.0+incompatible // indirect
+	github.com/otiai10/gosseract v2.2.1+incompatible
+	github.com/pdfcpu/pdfcpu v0.9.1
+	github.com/unidoc/unipdf/v3 v3.65.0
+	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50 // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/otiai10/gosseract"
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/extractor" // Import the extractor package from unipdf
+	"github.com/unidoc/unipdf/v3/model"     // Use unipdf to access NewReader, pdf functionality
+)
+
+func main() {
+	// Load the license for unipdf (apply your license key and license ID)
+	err := license.SetLicenseKey("your-license-key", "your-license-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the PDF file
+	file, err := os.Open("input.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Load the PDF document
+	pdfReader, err := model.NewPdfReader(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an OCR client (Tesseract)
+	client := gosseract.New
+	defer client.Close()
+
+	// Extract text and images from each page
+	for i := 0; i < pdfReader.NumPages(); i++ {
+		page, err := pdfReader.GetPage(i + 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Extract text using extractor
+		extractor, err := extractor.New(page)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		text, err := extractor.ExtractText()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Print extracted text from the page
+		fmt.Println("Page Text:", text)
+
+		// Extract images from the PDF page and run OCR if needed
+		// Note: You will need a method to extract and process images
+		// The unipdf package itself doesn't provide image extraction directly.
+		// You can convert pages to images or use other external tools (like ImageMagick or Ghostscript)
+		// Example of using OCR on an image:
+		// For now, this is a placeholder for OCR on images (assuming you have images from the PDF)
+		// ocrText, err := client.TextFromImage("image.png")
+		// if err == nil {
+		//     fmt.Println("OCR Text:", ocrText)
+		// }
+	}
+}
